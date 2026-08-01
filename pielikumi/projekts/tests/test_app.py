c56a78b8e3733a2032ec9aaeb363cf22a943ddfc
Sergejs Miļutins <sergejsmilutins1989@gmail.com>
2026-08-01 14:51:43 +0300

    Fix tests function name

--- pielikumi/projekts/tests/test_app.py
+++ pielikumi/projekts/tests/test_app.py
@@ -3,16 +3,16 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from app import saskaitit
+from app import add
 
 
 def test_add():
-    assert saskaitit(2, 3) == 5
+    assert add(2, 3) == 5
 
 
 def test_add_zero():
-    assert saskaitit(0, 5) == 5
+    assert add(0, 5) == 5
 
 
 def test_add_negative():
-    assert saskaitit(-2, -3) == -5
+    assert add(-2, -3) == -5
